94b560cbfede6d49f47788b1e299591e03b27cda
ImohROton <imohroton@gmail.com>
2025-09-17 20:00:35 -0400

    Draft for shift cypher. Added plaintext file to encrypt.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!";
+    string plaintext;
+
+    ifstream MyReadFile("input/plaintext.txt");
+
+    while (getline(MyReadFile, plaintext))
+    {
+        cout << plaintext << endl;
+    }
+
+    MyReadFile.close();
+
+    for (int i = 0; i < plaintext.length(); i++)
+    {
+        plaintext[i] += 1;
+    }
+
+    cout << plaintext << endl;
 }
